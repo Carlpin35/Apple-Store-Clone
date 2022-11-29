f693f4974c42409a8c097d7cc2870adaaabe3a58
Carlpin35 <carlpin800@gmail.com>
2022-11-29 20:45:05 -0300

    5b

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -21,7 +21,7 @@ export default function Checkout() {
 	const [loading, setLoading] = useState(false);
 
 	  useEffect(() => {
-    const groupedItems = items.reduce((results, item) => {
+    const groupedItems = items.reduce((results: number, item) => {
       (results[item._id] = results[item._id] || []).push(item);
       return results;
     }, {} as { [key: string]: Product[] });
